534c375ac137deb19bd55401519582cb2d2afa10
Kurokabe <farid.abdalla.13@gmail.com>
2024-03-20 06:10:07 +0100

    fix: hard code transformer engine path for now

--- transformer_engine/common/__init__.py
+++ transformer_engine/common/__init__.py
@@ -13,10 +13,11 @@ import sys
 def get_te_path():
     """Find Transformer Engine install path using pip"""
 
-    command = [sys.executable, "-m", "pip", "show", "transformer_engine"]
-    result = subprocess.run(command, capture_output=True, check=True, text=True)
-    result = result.stdout.replace("\n", ":").split(":")
-    return result[result.index("Location") + 1].strip()
+    # command = [sys.executable, "-m", "pip", "show", "transformer_engine"]
+    # result = subprocess.run(command, capture_output=True, check=True, text=True)
+    # result = result.stdout.replace("\n", ":").split(":")
+    # return result[result.index("Location") + 1].strip()
+    return "/workspaces/mugiwara/.venv/lib/python3.10/site-packages/transformer_engine"
 
 
 def _load_library():
